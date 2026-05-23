be7e384520adde349b3314fe464571782c500c01
Cristian Trujillo <cristiandanilotrujillo123@gmail.com>
2026-05-23 10:34:06 -0500

    Change default broker in app_strings to HiveMQ public broker

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,7 +1,7 @@
 class AppStrings {
   static const appName = 'NexusLED';
   static const tagline = 'Control. Anywhere. Anytime.';
-  static const defaultBroker = 'broker.emqx.io';
+  static const defaultBroker = 'broker.hivemq.com';
   static const defaultControlTopic = 'nexusled/led/control';
   static const defaultStatusTopic = 'nexusled/led/status';
 }
